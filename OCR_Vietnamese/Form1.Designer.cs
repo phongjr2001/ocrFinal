482da25f959c4232e19bb3448e16652051216c96
phongjr2001 <vudinhphong982001@gmail.com>
2023-03-15 19:49:50 +0700

    ocr

---
+++ OCR_Vietnamese/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace OCR_Vietnamese
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_choose = new System.Windows.Forms.Button();
+            this.btn_ocr = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picloading = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_takephoto = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btn_saveresult = new System.Windows.Forms.Button();
+            this.crop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picloading)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_choose
+            // 
+            this.btn_choose.Location = new System.Drawing.Point(495, 54);
+            this.btn_choose.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_choose.Name = "btn_choose";
+            this.btn_choose.Size = new System.Drawing.Size(119, 28);
+            this.btn_choose.TabIndex = 1;
+            this.btn_choose.Text = "Choose Image";
+            this.btn_choose.UseVisualStyleBackColor = true;
+            this.btn_choose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_ocr
+            // 
+            this.btn_ocr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ocr.ForeColor = System.Drawing.Color.Red;
+            this.btn_ocr.Location = new System.Drawing.Point(637, 313);
+            this.btn_ocr.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_ocr.Name = "btn_ocr";
+            this.btn_ocr.Size = new System.Drawing.Size(123, 110);
+            this.btn_ocr.TabIndex = 2;
+            this.btn_ocr.Text = "OCR";
+            this.btn_ocr.UseVisualStyleBackColor = true;
+            this.btn_ocr.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.richTextBox1.Location = new System.Drawing.Point(787, 90);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(647, 574);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(783, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Result";
+            // 
+            // picloading
+            // 
+            this.picloading.Image = global::OCR_Vietnamese.Properties.Resources.loading;
+            this.picloading.Location = new System.Drawing.Point(1040, 283);
+            this.picloading.Margin = new System.Windows.Forms.Padding(4);
+            this.picloading.Name = "picloading";
+            this.picloading.Size = new System.Drawing.Size(143, 130);
+            this.picloading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picloading.TabIndex = 7;
+            this.picloading.TabStop = false;
+            this.picloading.Visible = false;
+            this.picloading.Click += new System.EventHandler(this.picloading_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 90);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(597, 574);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(16, 54);
+            this.btn_start.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(100, 28);
+            this.btn_start.TabIndex = 8;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_takephoto
+            // 
+            this.btn_takephoto.Location = new System.Drawing.Point(279, 54);
+            this.btn_takephoto.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_takephoto.Name = "btn_takephoto";
+            this.btn_takephoto.Size = new System.Drawing.Size(100, 28);
+            this.btn_takephoto.TabIndex = 9;
+            this.btn_takephoto.Text = "Take photo";
+            this.btn_takephoto.UseVisualStyleBackColor = true;
+            this.btn_takephoto.Click += new System.EventHandler(this.btn_takephoto_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(387, 54);
+            this.btn_save.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(100, 28);
+            this.btn_save.TabIndex = 10;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(124, 54);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 24);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Title = "ImageName|*.png";
+            // 
+            // btn_saveresult
+            // 
+            this.btn_saveresult.Location = new System.Drawing.Point(1335, 54);
+            this.btn_saveresult.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_saveresult.Name = "btn_saveresult";
+            this.btn_saveresult.Size = new System.Drawing.Size(100, 28);
+            this.btn_saveresult.TabIndex = 12;
+            this.btn_saveresult.Text = "Save";
+            this.btn_saveresult.UseVisualStyleBackColor = true;
+            this.btn_saveresult.Click += new System.EventHandler(this.btn_saveresult_Click);
+            // 
+            // crop
+            // 
+            this.crop.Location = new System.Drawing.Point(659, 54);
+            this.crop.Name = "crop";
+            this.crop.Size = new System.Drawing.Size(77, 47);
+            this.crop.TabIndex = 13;
+            this.crop.Text = "crop";
+            this.crop.UseVisualStyleBackColor = true;
+            this.crop.Click += new System.EventHandler(this.Crop);
+            this.crop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.crop.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(659, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 63);
+            this.button1.TabIndex = 14;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(659, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 58);
+            this.button2.TabIndex = 15;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1451, 679);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.crop);
+            this.Controls.Add(this.btn_saveresult);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_takephoto);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.picloading);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_ocr);
+            this.Controls.Add(this.btn_choose);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "[C#] Convert Image To Text";
+            ((System.ComponentModel.ISupportInitialize)(this.picloading)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_choose;
+        private System.Windows.Forms.Button btn_ocr;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picloading;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_takephoto;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btn_saveresult;
+        private System.Windows.Forms.Button crop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OCR_Vietnamese/Form1.cs
@@ -0,0 +1,217 @@
+﻿using AForge.Video.DirectShow;      //camera
+using System;
+using System.Diagnostics.Tracing;
+using System.Drawing;   //bitmap
+using System.Net.NetworkInformation;
+using System.Threading.Tasks; //luồng
+using System.Windows.Forms;
+using Tesseract;    //ocr
+
+namespace OCR_Vietnamese
+{
+
+
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection cameras;   //liệt kê các thiết bị video
+        private VideoCaptureDevice cam; //nguồn video của cam, ghi lại dữ liệu video từ camera
+        int xDown = 0;
+        int yDown = 0;
+        int xUp = 0;
+        int yUp = 0;
+        Rectangle rectCropArea = new Rectangle();
+        System.IO.MemoryStream ms = new System.IO.MemoryStream();
+        Task timeout;
+        string fn = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            //liệt kê các camera mà máy sử dụng r hiển thị ra combobox cho ng dùng chọn
+            cameras = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo info in cameras)
+            {
+                comboBox1.Items.Add(info.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            pictureBox1.Cursor = Cursors.Cross;
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //pictureBox1.Image.Clone();
+            xUp = e.X;
+            yUp = e.Y;
+            Rectangle rec = new Rectangle(xDown, yDown, Math.Abs(xUp - xDown), Math.Abs(yUp - yDown));
+            using (Pen pen = new Pen(Color.Black, 3))
+            {
+
+                pictureBox1.CreateGraphics().DrawRectangle(pen, rec);
+            }
+            rectCropArea = rec;
+            crop.Enabled = true;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Invalidate();
+
+            xDown = e.X;
+            yDown = e.Y;
+            crop.Enabled = true;
+        }
+
+        //mở file, sử dụng openFileDialog
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.LoadAsync(openFileDialog.FileName);
+            }
+        }
+
+        //khởi động webcam
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (cam != null && cam.IsRunning)
+            {
+                cam.Stop();
+            }
+            cam = new VideoCaptureDevice(cameras[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += Cam_NewFrame;
+            cam.Start();
+        }
+
+        private void Cam_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bitmap;
+        }
+
+        //chụp ảnh
+        private void btn_takephoto_Click(object sender, EventArgs e)
+        {
+            if (cam != null && cam.IsRunning)
+            {
+                cam.Stop();
+            }
+        }
+
+        //lưu ảnh
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = "F:\\Thực tập cs cn\\Images";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        //chuyển ảnh qua text
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            Task.Factory.StartNew(() =>
+            {
+                picloading.BeginInvoke(new Action(() =>
+                {
+                    picloading.Visible = true;
+                }));
+
+                //thực hiện convert
+                result = OCR((Bitmap)pictureBox1.Image);
+                richTextBox1.BeginInvoke(new Action(() =>
+                {
+
+                    richTextBox1.Text = result;
+
+                }));
+                picloading.BeginInvoke(new Action(() =>
+                {
+                    picloading.Visible = false;
+                }));
+
+            });
+        }
+
+        //convert
+        private string OCR(Bitmap b)
+        {
+            string res = "";
+            using (var engine = new TesseractEngine(@"tessdata", "vie", EngineMode.Default))
+            {
+                using (var page = engine.Process(b, PageSegMode.AutoOnly))
+                    res = page.GetText();
+            }
+            return res;
+        }
+
+        private void picloading_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_saveresult_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)";
+            saveFileDialog.AddExtension = true;
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var extension = System.IO.Path.GetExtension(saveFileDialog.FileName);
+                if (extension.ToLower() == ".txt") /*saveFileDialog.FilterIndex==1*/
+                    richTextBox1.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                else
+                    richTextBox1.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.RichText);
+            }
+        }
+
+        private void Crop(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Refresh();
+                //Prepare a new Bitmap on which the cropped image will be drawn
+                Bitmap sourceBitmap = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+                Graphics g = pictureBox1.CreateGraphics();
+
+                //Draw the image on the Graphics object with the new dimesions
+                g.DrawImage(sourceBitmap, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height), rectCropArea, GraphicsUnit.Pixel);
+                sourceBitmap.Dispose();
+                crop.Enabled = false;
+                var path = Environment.CurrentDirectory.ToString();
+                ms = new System.IO.MemoryStream();
+               
+               
+                pictureBox1.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+               
+                byte[] ar = new byte[ms.Length];
+                var timeout = ms.WriteAsync(ar, 0, ar.Length);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                var imagerl = pictureBox1.Image;
+                imagerl.RotateFlip(System.Drawing.RotateFlipType.Rotate90FlipXY);
+                pictureBox1.Image = imagerl;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                var imagerl = pictureBox1.Image;
+                imagerl.RotateFlip(System.Drawing.RotateFlipType.Rotate90FlipXY);
+                pictureBox1.Image = imagerl;
+            }
+        }
+    }
+}
